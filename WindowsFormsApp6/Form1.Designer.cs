15c4c9c9bba0c7a967b75434f8fb27454e38e7dd
newviplayer <mxs9fp@gmail.com>
2024-03-11 12:34:19 +0900

    add find color

--- WindowsFormsApp6/Form1.Designer.cs
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -28,40 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.열기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectShapeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 63);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 78);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(680, 481);
+            this.pictureBox1.Size = new System.Drawing.Size(724, 443);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(698, 63);
+            this.pictureBox2.Location = new System.Drawing.Point(783, 78);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(680, 481);
+            this.pictureBox2.Size = new System.Drawing.Size(724, 443);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
@@ -69,7 +64,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(700, 601);
+            this.label1.Location = new System.Drawing.Point(781, 540);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 12);
             this.label1.TabIndex = 4;
@@ -78,8 +73,9 @@
             // 
             // trackBar1
             // 
+            this.trackBar1.Enabled = false;
             this.trackBar1.LargeChange = 10;
-            this.trackBar1.Location = new System.Drawing.Point(698, 12);
+            this.trackBar1.Location = new System.Drawing.Point(698, 27);
             this.trackBar1.Maximum = 100;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(680, 45);
@@ -87,34 +83,81 @@
             this.trackBar1.TickFrequency = 5;
             this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1533, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.열기ToolStripMenuItem});
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.testToolStripMenuItem.Text = "파일";
+            // 
+            // 열기ToolStripMenuItem
+            // 
+            this.열기ToolStripMenuItem.Name = "열기ToolStripMenuItem";
+            this.열기ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.열기ToolStripMenuItem.Text = "열기";
+            this.열기ToolStripMenuItem.Click += new System.EventHandler(this.열기ToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.detectShapeToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(43, 20);
+            this.toolStripMenuItem1.Text = "선택";
+            // 
+            // detectShapeToolStripMenuItem
+            // 
+            this.detectShapeToolStripMenuItem.Name = "detectShapeToolStripMenuItem";
+            this.detectShapeToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.detectShapeToolStripMenuItem.Text = "detect shape";
+            this.detectShapeToolStripMenuItem.Click += new System.EventHandler(this.detectShapeToolStripMenuItem_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1394, 712);
+            this.ClientSize = new System.Drawing.Size(1533, 565);
+            this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 열기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectShapeToolStripMenuItem;
     }
 }
 
--- WindowsFormsApp6/Form1.cs
+++ WindowsFormsApp6/Form1.cs
@@ -20,11 +20,20 @@ namespace WindowsFormsApp6
         {
             
         }
-
+        public string test(Vec3b aaa)
+        {
+            if (aaa.Item2 > aaa.Item1 && aaa.Item2 > aaa.Item0)
+                return "Red";
+            else if (aaa.Item1 > aaa.Item0 && aaa.Item1 > aaa.Item2)
+                return "Green";
+            else
+                return "Blue";
+        }
         public void img_process(double treshold_val)
         {
             Mat find_shape = img.Clone();
-
+            Console.WriteLine(find_shape.Height);
+            Console.WriteLine(find_shape.Width);
             Mat bin = new Mat();
             Cv2.CvtColor(img, bin, ColorConversionCodes.BGR2GRAY);
             Cv2.Threshold(bin, bin, treshold_val, 255, ThresholdTypes.BinaryInv);
@@ -44,16 +53,25 @@ namespace WindowsFormsApp6
 
                 if (area > 5000)
                 {
+                    Console.WriteLine(shape_num);
                     Console.WriteLine(area);
                     new_contours.Add(contour);
-                    Cv2.DrawContours(find_shape, new Point[][] { contour },-1, Scalar.Gray, 3);
+                    Cv2.DrawContours(find_shape, new Point[][] { contour },-1, Scalar.DarkGray, 3);
                     int Cx = (int)(moments.M10 / moments.M00);
                     int Cy = (int)(moments.M01 / moments.M00);
-                    Cv2.Circle(find_shape, Cx, Cy, 5, Scalar.Black, -1);
-                    Cv2.PutText(find_shape, shape_num.ToString(), new Point(Cx - 60, Cy + 25), HersheyFonts.HersheySimplex,1,Scalar.Black,1);
+                    //Console.WriteLine(Cx + " ," + Cy);
+                    Vec3b pixelValue = img.At<Vec3b>(Cy, Cx);
+                    //Console.WriteLine(pixelValue);
+                    string what_color = test(pixelValue);
+                    //int hue = GetHue(pixelValue.Item2, pixelValue.Item1, pixelValue.Item0);
+                    //Console.WriteLine(hue);
+                    Cv2.Circle(find_shape, Cx, Cy, 1, Scalar.Black, -1);
+                    //Cv2.PutText(find_shape, shape_num.ToString(), new Point(Cx - 60, Cy), HersheyFonts.HersheySimplex, 1, Scalar.Black, 2);
+                    Cv2.PutText(find_shape, what_color, new Point(Cx - 60, Cy + 25), HersheyFonts.HersheySimplex, 1, Scalar.Black, 2);
                     shape_num++;
                 }
             }
+
             cnt = new_contours.Count;
 
             pictureBox2.Image = BitmapConverter.ToBitmap(find_shape);
@@ -66,8 +84,7 @@ namespace WindowsFormsApp6
                 label1.Text = "find " + cnt + " shape";
             }
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        public void open_image()
         {
             OpenFileDialog dlg = new OpenFileDialog();
 
@@ -75,19 +92,58 @@ namespace WindowsFormsApp6
             {
                 img_src = dlg.FileName;
                 img = new Mat(img_src);
+                Console.WriteLine(img.Height + ",   " + img.Width);
                 pictureBox1.Image = BitmapConverter.ToBitmap(img);
-                trackBar1.Value = (int)(240 / 2.55);
-                img_process(240);
             }
-            label1.Visible = true;
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
             double threshval = trackBar1.Value * 2.55;
-
+            Console.WriteLine(threshval);
             img_process(threshval);
         }
 
+        private void 열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {         
+            open_image();
+            pictureBox2.Image = null;
+            trackBar1.Enabled = false;
+            label1.Visible = false;
+        }
+
+        private void detectShapeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            trackBar1.Value = (int)(120 / 2.55);
+            img_process(160.65);
+            trackBar1.Enabled = true;
+            label1.Visible = true;
+        }
     }
 }
+
+//1
+//50원
+//17129.5
+//2
+//500원
+//247352
+//3
+//500원
+//234243.5
+//4
+//100원
+//195466.5
+//5
+//신 10원
+//104327.5
+//6
+//구 10원
+//157666.5
+//7
+//100원
+//188179.5
+//8
+//신 10원
+//101443
+
